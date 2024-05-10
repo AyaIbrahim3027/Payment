c0b2bab824403b2e2799bf8e637c2863588aac26
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-05-10 16:18:36 +0200

    Implement init payment sheet

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:payment/core/utils/api_keys.dart';
 import 'package:payment/core/utils/api_service.dart';
 import 'package:payment/features/checkout/data/models/payment_intent_input_model.dart';
@@ -7,7 +8,7 @@ import '../../features/checkout/data/models/payment_intent_model/payment_intent_
 class StripeService {
   final ApiService apiService = ApiService();
   Future<PaymentIntentModel> createPaymentIntent(
-      PaymentIntentInputModel paymentIntentInputModel) async{
+      PaymentIntentInputModel paymentIntentInputModel) async {
     var response = await apiService.post(
         body: paymentIntentInputModel.toJson(),
         url: 'https://api.stripe.com/v1/payment_intents',
@@ -16,5 +17,13 @@ class StripeService {
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
 
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+        paymentSheetParameters:  SetupPaymentSheetParameters(
+          paymentIntentClientSecret: paymentIntentClientSecret,
+          merchantDisplayName: "Aya"
+        ));
   }
 }
